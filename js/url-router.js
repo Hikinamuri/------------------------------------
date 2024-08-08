07d3a72b5267a84f0de8ed463bb4e6b679760c03
Hikinamuri <Hikinamuri@mail.ru>
2024-08-08 03:54:06 +0300

    update: sidebar styles

--- js/url-router.js
+++ js/url-router.js
@@ -1,12 +1,12 @@
 const urlPageTitle = 'Тестовое задание'
 
 document.addEventListener('click', (e) => {
-    if(!e.target.matches('nav a')) {
+    if(!e.target.matches('div')) {
         return;
     }
-    console.log(e)
-    e.preventDefault();
-    urlRoute(e);
+
+    const href = e.target.getAttribute('data-href');
+    urlLocationHandler(href)
 })
 
 const urlRoutes = {
@@ -23,33 +23,24 @@ const urlRoutes = {
     '/locations' : {
         template: '/templates/locations.html',
         title: 'Home | ' + urlPageTitle,
-        descripytion: 'Это домашняя страницыа',
+        descripytion: 'Это домашняя страница',
+        link: 'Локации'
     },
     '/settings' : {
         template: '/templates/settings.html',
         title: 'About | ' + urlPageTitle,
         descripytion: 'Это познавательная страницыа',
+        link: 'Настройки'
     }
 }
 
-const urlRoute = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, '', event.target.href)
-    urlLocationHandler()
-}
-
-const urlLocationHandler = async () => {
-    const location = window.location.pathname;
-    console.log(location)
-    if (location.length == 0) {
-        location = '/'
-    }
-
-    const route = urlRoutes[location] || urlRoutes[404]
-    console.log(route, 'route')
+const urlLocationHandler = async (href) => {
+    const route = urlRoutes[href] || urlRoutes[404]
     const html = await fetch(route.template).then(response => response.text())
-    document.getElementById('content').innerHTML = html
+    const pageName = route.link || route.title;
+
+    document.getElementById('htmlContent').innerHTML = html
+    document.getElementById('contentName').innerText = pageName;
     document.title = route.title;
     document
         .querySelector('meta[name="description"]')
